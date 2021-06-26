1277aa4360c4b778819455788f5bdd1e9736c497
Pongsak Srithongnopawong <pongsaks@Pongsaks-MacBook-Air.local>
2021-06-26 11:11:14 +0700

    wrap up the simple chain code

--- excercise/private_blockchain/simpleChain.js
+++ excercise/private_blockchain/simpleChain.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block{
 	constructor(data){
      this.hash = "";
@@ -11,9 +13,16 @@ class Block{
 class Blockchain {
     constructor() {
         this.chain = [];
+        this.addBlock(new Block("First block in the chain = Genesis block"))
     }
 
     addBlock(newBlock){
+        newBlock.height = this.chain.length;
+        newBlock.time = new Date().getTime().toString().slice(0,-3);
+        if(this.chain.length > 0) {
+            newBlock.previousblockhash = this.chain[this.chain.length - 1].hash;
+        }
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
         this.chain.push(newBlock);
     }
 
