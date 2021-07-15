9b860df057058a863736c1a2b1485dd24f17419c
Pongsak Srithongnopawong <pongsaks@Pongsaks-MacBook-Air.local>
2021-07-16 01:35:02 +0700

    wrap up solidity excercise

---
+++ excercise/solidity/truffle-config.js
@@ -0,0 +1,30 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "your infura key here";
+//
+// const fs = require('fs');
+const mnemonic = "your meta mask seed here";
+
+module.exports = {
+
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  }
+
+}
