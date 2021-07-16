a3f3a65714fba69fc5ac187e1e47574567dee66a
Pongsak Srithongnopawong <pongsaks@Pongsaks-MacBook-Air.local>
2021-07-16 23:33:39 +0700

    add test for starnotaryv1 excercise

--- excercise/starNotaryv1/test/starNotary.js
+++ excercise/starNotaryv1/test/starNotary.js
@@ -12,6 +12,28 @@ contract('StarNotary', (accs) => {
 it('has correct name', async () => {
 	let instance = await StarNotary.deployed();
 	let starName = await instance.starName.call();
-	
+
 	assert.equal(starName, 'Awesome Udacity Star');
 });
+
+it('can be claimed', async () => {
+	let instance = await StarNotary.deployed();
+	await instance.claimStar({from: owner});
+	let starOwner = await instance.starOwner.call();
+	assert.equal(starOwner, owner);
+});
+
+it('can change owners', async () => {
+
+	let instance = await StarNotary.deployed();
+
+	let secondUser = accounts[1];
+	await instance.claimStar({ from: owner });
+	let starOwner = await instance.starOwner.call();
+	assert.equal(starOwner, owner);
+
+	await instance.claimStar({ from: secondUser });
+	let secondOwner = await instance.starOwner.call();
+	assert.equal(secondOwner, secondUser);
+});
+
