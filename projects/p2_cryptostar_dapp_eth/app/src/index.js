38e9a61abf1374560f20c41b9865b6d98a6e8295
Pongsak Srithongnopawong <pongsaks@Pongsaks-MacBook-Air.local>
2021-07-18 14:34:32 +0700

    adjust config

--- projects/p2_cryptostar_dapp_eth/app/src/index.js
+++ projects/p2_cryptostar_dapp_eth/app/src/index.js
@@ -37,11 +37,6 @@ const App = {
     const id = document.getElementById("starId").value;
     await createStar(name, id).send({from: this.account});
     App.setStatus("New Star Owner is " + this.account + ".");
-  },
-
-  // Implement Task 4 Modify the front end of the DAPP
-  lookUp: async function (){
-    
   }
 
 };
@@ -54,9 +49,9 @@ window.addEventListener("load", async function() {
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:7545. You should remove this fallback when you deploy live",);
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"),);
   }
 
   App.start();
