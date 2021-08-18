8a71bca601447b45c2c873ab526a3e0b19a93756
Pongsak Srithongnopawong <pongsaks@Pongsaks-MacBook-Air.local>
2021-08-18 21:38:14 +0700

    excercise complete the pausing a smart contract with stoploss

---
+++ excercise/BCND-C6-Exercises-ExerciseC6A/truffle.js
@@ -0,0 +1,16 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// Be sure to match this mnemonic with that in Ganache!
+var mnemonic = "Mnemonic: fabric blast globe wise cruise profit file sword bracket patch aunt rabbit";
+
+module.exports = {
+  networks: {
+    development: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:9545/", 0, 10);
+      },
+      network_id: '*',
+      gas: 9999999
+    }
+  }
+};
